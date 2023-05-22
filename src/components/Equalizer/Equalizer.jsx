a2e54b3a28d795574dbf1cfa7175a20866d01dac
Денис Усов <Denis.usov@ukr.net>
2023-05-22 22:10:49 +0300

    add equalizer

---
+++ src/components/Equalizer/Equalizer.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import s from './Equalizer.module.css';
+
+const Equalizer = () => {
+  const [barHeight, setBarHeight] = useState([50, 75, 100]); // Начальные значения высоты столбиков
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newHeight = barHeight.map(() => Math.random() * 100); // Генерация случайных значений высоты
+      setBarHeight(newHeight);
+    }, 150); // Интервал обновления значений (в миллисекундах)
+
+    return () => clearInterval(interval);
+  }, [barHeight]);
+
+  return (
+    <div className={s.equalizerBlock}>
+        <div className={s.equalizerHeader}>
+            <div className={s.equalizerName}>JellyFish</div>
+            <div className={s.equalizer}>
+                {barHeight.map((height, index) => (
+                    <div key={index} className={s.bar} style={{ height: `${height}%` }} />
+                ))}
+            </div>
+            <div className={s.equalizerText}>OCEAN</div>
+        </div>
+        <div className={s.description}>
+            <div className={s.production}>Production - VICI</div>
+            <div className={s.director}>Director - MELNYK KOSTYA</div>
+        </div>
+    </div>
+    
+  );
+};
+
+export default Equalizer;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import Header from '../../components/Header';
 import SocialMedia from '../../components/SocialMedia';
+import Equalizer from '../../components/Equalizer';
 import s from './HomePage.module.css'
 
 
@@ -9,6 +10,7 @@ export default function HomePage() {
         <div className={s.home}>
             <Header/>
             <SocialMedia/>
+            <Equalizer/>
         </div>
     )
 }
